ed379069152621fd509cc9ef45160ce3dd3e7083
Chanho Jung <jungch@naver.com>
2022-05-04 23:38:39 +0900

    fix

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1077,7 +1077,7 @@ static void bb_ui_draw_measures_right(UIState *s, int bb_x, int bb_y, int bb_w )
     char uom_str[6];
     NVGcolor val_color = COLOR_WHITE_ALPHA(200);
     if(scene.engine_rpm == 0) {
-       snprintf(val_str, sizeof(val_str), "Off");
+       snprintf(val_str, sizeof(val_str), "OFF");
     } else {
       if(scene.engine_rpm > 3000) {
         val_color = COLOR_RED_ALPHA(200);
@@ -1097,7 +1097,7 @@ static void bb_ui_draw_measures_right(UIState *s, int bb_x, int bb_y, int bb_w )
 
   //finally draw the frame
   nvgBeginPath(s->vg);
-  nvgRoundedRect(s->vg, bb_x, bb_y, bb_w, bb_ry - bb_y + 45, 20);
+  nvgRoundedRect(s->vg, bb_x, bb_y - 20, bb_w, bb_ry - bb_y + 45, 20);
   nvgStrokeColor(s->vg, COLOR_WHITE_ALPHA(80));
   nvgStrokeWidth(s->vg, 6);
   nvgStroke(s->vg);
