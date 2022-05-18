66a16ac9ccf2e5aae7acff742be57adeade39965
Chanho Jung <jungch@naver.com>
2022-05-18 20:06:06 +0900

    Revert "Neokii tune"
    This reverts commit 2c4da4b05f86a9335a73cec13c85aa3a7cd5bc26.

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -111,16 +111,10 @@ class LatControlTorque(LatControl):
       # convert friction into lateral accel units for feedforward
       friction_compensation = interp(desired_lateral_jerk, [-JERK_THRESHOLD, JERK_THRESHOLD], [-self.friction, self.friction])
       ff += friction_compensation / self.kf
-
-      # Neokii
-      # Prevent integrator windup at very low speed
-      # or when steering is limited
-      freeze_integrator = CS.steeringRateLimited or CS.vEgo < 5
-
       output_torque = self.pid.update(error_deadzone,
                                       override=CS.steeringPressed, feedforward=ff,
                                       speed=CS.vEgo,
-                                      freeze_integrator=freeze_integrator)
+                                      freeze_integrator=CS.steeringRateLimited)
 
       pid_log.active = True
       pid_log.p = self.pid.p
