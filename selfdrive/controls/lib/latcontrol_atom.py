e3bd19c6527ece528948cf28831210d2e8cfafb4
정찬호(IT전략기획담당) <jungch@naver.com>
2022-05-23 08:10:47 +0900

    Revert "hoya tune for ATOM hybrid"
    This reverts commit 2a445b2014eabc6ca849a93d6d6054cd2f08f4f2.

--- selfdrive/controls/lib/latcontrol_atom.py
+++ selfdrive/controls/lib/latcontrol_atom.py
@@ -109,7 +109,7 @@ class LatControlATOM(LatControl):
       lqr_output_torque, lqr_desired_angle, lqr_log  = self.LaLqr.update( active, CS, CP, VM, params, last_actuators, desired_curvature, desired_curvature_rate, llk )
       toq_output_torque, toq_desired_angle, toq_log  = self.LaToq.update( active, CS, CP, VM, params, last_actuators, desired_curvature, desired_curvature_rate, llk )
 
-      if self.torqueMaxSpeed < CS.vEgo and CS.vEgo < 120 :  
+      if CS.vEgo < self.torqueMaxSpeed:  # 12.5 45 kph
         selected = 1.0  # toq
       else:
         #output_torque = lqr_output_torque
